cd03bb7272e0df847cdb5729d248dab2747e58a4
manishmuchhadiya <manishmuchhadiya8@gmail.com>
2025-09-30 12:46:58 +0530

    learn new'

--- Day_7/practic.js
+++ Day_7/practic.js
@@ -8,33 +8,86 @@
 Slove using callback then promisce and async await
 */
 
-//  Callbacks
+//  Callbacks  ===================================================================================
 
-const register = (callback) => {
-  setTimeout(() => {
-    console.log("   - 1.Register ");
-    callback();
-  }, 5000);
+// const register = (callback) => {
+//   setTimeout(() => {
+//     console.log("   - 1.Register ");
+//     callback();
+//   }, 5000);
+// };
+
+// const sendEmail = (callback) => {
+//   setTimeout(() => {
+//     console.log("   - 2.Email Send");
+//     callback();
+//   }, 4000);
+// };
+
+// const logIn = (callback) => {
+//   setTimeout(() => {
+//     console.log("   - 3.Login succesful ");
+//     callback();
+//   }, 3000);
+// };
+
+// const getUserData = (callback) => {
+//   setTimeout(() => {
+//     console.log("   - 4.Got user data");
+//     callback();
+//   }, 2000);
+// };
+
+// const displayUserData = () => {
+//   setTimeout(() => {
+//     console.log("   - 5.User data display");
+//   }, 1000);
+// };
+
+// register(function () {
+//   sendEmail(function () {
+//     logIn(function () {
+//       getUserData(function () {
+//         displayUserData();
+//       });
+//     });
+//   });
+// });
+
+// console.log("  Last But First");
+
+// Promises  ============================================================================================
+
+const register = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("   - 1.Register");
+      resolve();
+    }, 5000);
+  });
 };
 
-const sendEmail = (callback) => {
-  setTimeout(() => {
-    console.log("   - 2.Email Send");
-    callback();
-  }, 4000);
+const sendEmail = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("   - 2.Email send");
+      resolve();
+    }, 4000);
+  });
 };
 
-const logIn = (callback) => {
-  setTimeout(() => {
-    console.log("   - 3.Login succesful ");
-    callback();
-  }, 3000);
+const logIn = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("   - 3.Login succesful");
+      reject("error at login");
+    }, 3000);
+  });
 };
 
-const getUserData = (callback) => {
+const getUserData = () => {
   setTimeout(() => {
     console.log("   - 4.Got user data");
-    callback();
   }, 2000);
 };
 
@@ -44,18 +97,17 @@ const displayUserData = () => {
   }, 1000);
 };
 
-register(function () {
-  sendEmail(function () {
-    logIn(function () {
-      getUserData(function () {
-        displayUserData();
-      });
-    });
-  });
-});
+register()
+  .then(sendEmail)
+  .then(logIn)
+  .then(getUserData)
+  .then(displayUserData)
+  .catch((err) => console.log(err));
+// sendEmail();
+// logIn();
+// getUserData();
+// displayUserData();
 
 console.log("  Last But First");
 
-//Promises
-
-//Async Await
+// Async Await ==========================================================================================
