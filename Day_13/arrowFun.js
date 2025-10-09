eac16a39ef643a22566f8da7ebb2cf81462e53fa
manishmuchhadiya <manishmuchhadiya8@gmail.com>
2025-10-09 13:59:43 +0530

    add day 13

---
+++ Day_13/arrowFun.js
@@ -0,0 +1,25 @@
+// const btn = document.querySelector("button");
+
+// give us out put we need
+// const shop = {
+//   price: 100,
+//   buy: function () {
+//     btn.addEventListener("click", () => {
+//       console.log("i spend " + this.price);
+//     });
+//   },
+// };
+//shop.buy();
+
+// not working unlees you use self
+// const shop = {
+//   price: 100,
+//   buy: function () {
+//     let self = this;
+//     btn.addEventListener("click", function () {
+//       console.log("i spend " + this.price); // give us a undifined
+//       console.log("i spend " + self.price); // give us a output
+//     });
+//   },
+// };
+// shop.buy();
