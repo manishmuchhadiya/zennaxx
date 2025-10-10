34c0f0452f867e9d74ef8b8ce9e4b5cd60280024
manishmuchhadiya <manishmuchhadiya8@gmail.com>
2025-10-10 10:42:19 +0530

    Added day 13

---
+++ Day_13/es6.js
@@ -0,0 +1,26 @@
+//spred ...
+// const setting = {
+//   volum: 10,
+//   brightness: 50,
+//   address: {
+//     street: "xyz",
+//   },
+// };
+
+// console.log(setting, `setting has printed `);
+// const newsetrign = { ...setting };
+// console.log(newsetrign, `new setting has printed `);
+
+//rest ...
+// const addition = (...items) => {
+//   console.log(items);
+// };
+// addition(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+//for of
+// use with array, string, object, set, map
+
+const array = [1, 2, 3, 4, 5];
+for (n of array) {
+  console.log(n);
+}
