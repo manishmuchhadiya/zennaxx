bac3d4e65b7a4392c737a74a987416bec94cefdc
manishmuchhadiya <manishmuchhadiya8@gmail.com>
2025-10-14 17:49:40 +0530

    Add 01-folder

---
+++ React js/01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h3>Shortcut to make Function in react</h3>
+      <h1>rafce : </h1>
+      <h2>React Arrow Function Export Component</h2>
+    </div>
+  );
+};
+
+export default App;
